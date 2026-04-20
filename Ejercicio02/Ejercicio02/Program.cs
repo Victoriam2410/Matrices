72e4343a32d1acce3d8388aa2cf8b3dfac9cfbcc
Victoriam2410 <estherrodas61@gmail.com>
2026-04-19 22:48:01 -0600

    Add files via upload

---
+++ Ejercicio02/Ejercicio02/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Ingrese la cantidad de filas: ");
+int filas = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la cantidad de columnas: ");
+int columnas = int.Parse(Console.ReadLine());
+int[,] matriz = new int[filas, columnas];
+
+for (int i = 0; i < filas; i++)
+{
+    for (int j = 0; j < columnas; j++)
+    {
+        Console.Write($"Ingrese el valor para [{i},{j}]: ");
+        matriz[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("Ingrese el número de fila que desea mostrar: ");
+int filaMostrar = int.Parse(Console.ReadLine());
+if (filaMostrar >= 0 && filaMostrar < filas)
+{
+    Console.WriteLine($"Valores de la fila {filaMostrar}:");
+    for (int j = 0; j < columnas; j++)
+    {
+        Console.Write(matriz[filaMostrar, j] + " ");
+    }
+}
+else
+{
+    Console.WriteLine("La fila ingresada no existe.");
+}
