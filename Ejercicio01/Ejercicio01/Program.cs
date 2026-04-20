202bf9bccee5b7e6ade0ba3e3babf63d1c59127b
Victoriam2410 <estherrodas61@gmail.com>
2026-04-19 22:11:53 -0600

    Add files via upload

---
+++ Ejercicio01/Ejercicio01/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Ingrese la cantidad de filas: ");
+int filas = int .Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la cantidad de columnas: ");
+int columnas = int.Parse(Console.ReadLine());
+int[,] matriz = new int[filas, columnas];
+
+for (int i =0; i < filas; i++)
+{
+    for (int j = 0; j < columnas; j++)
+    {
+        Console.Write("Ingrese los valores:");
+        matriz[i,j] = int.Parse(Console.ReadLine());
+    }
+}
+Console.WriteLine("La tabla es: ");
+for (int i = 0; i < filas; i++)
+{
+    for (int j = 0; j < columnas; j++)
+    {
+        Console.Write(matriz[i,j] + "|");
+    }
+    Console.WriteLine();
+}
