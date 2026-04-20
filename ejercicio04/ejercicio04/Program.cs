cab907ddefd21262ce7f1cfb63afac8e8de4d815
Victoriam2410 <estherrodas61@gmail.com>
2026-04-19 22:48:51 -0600

    Add files via upload

---
+++ ejercicio04/ejercicio04/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Ingrese la cantidad de filas: ");
+int filas = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la cantidad de columnas: ");
+int columnas = int.Parse(Console.ReadLine());
+int[,] matriz = new int[filas, columnas];
+
+for (int i = 0; i < filas; i++)
+{
+    for (int j = 0; j < columnas; j++)
+    {
+        Console.Write($"Ingrese el valor para [{i},{j}]: ");
+        matriz[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+int mayor = matriz[0, 0];
+
+for (int i = 0; i < filas; i++)
+{
+    for (int j = 0; j < columnas; j++)
+    {
+        if (matriz[i, j] > mayor)
+        {
+            mayor = matriz[i, j];
+        }
+    }
+}
+
+Console.WriteLine($"El numero mayor en la matriz es: {mayor}");
